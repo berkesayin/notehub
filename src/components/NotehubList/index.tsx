cf9dff01e25d21e2343cf32ed5df45df4621fc3c
Berke Sayın <sayinberke34@gmail.com>
2023-12-18 21:05:38 +0300

    List notes contents

--- src/components/NotehubList/index.tsx
+++ src/components/NotehubList/index.tsx
@@ -1,7 +1,25 @@
+import { useNotehubContext } from "../../contexts/useNotehubContext";
+import { SiStarship } from "react-icons/si";
+
 export const NotehubList = () => {
+  const { noteCards } = useNotehubContext();
+
+  if (!noteCards.length) {
+    return (
+      <div className="max-w-lg px-5 m-auto">
+        <h1 className="flex flex-col items-center gap-5 px-5 py-10 text-xl font-bold text-center rounded-xl bg-zinc-900">
+          <SiStarship className="text-5xl" />
+          There isn't any notes!
+        </h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <p>Note List Component</p>
-    </div>
+    <ul className="grid max-w-lg gap-2 px-5 m-auto">
+      {noteCards.map((noteCard) => (
+        <li key={noteCard}>{noteCard}</li>
+      ))}
+    </ul>
   );
 };
