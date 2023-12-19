9ea24b36a1f0d1ba332e66a7bcb27819aedcd854
Berke Sayın <sayinberke34@gmail.com>
2023-12-19 15:59:29 +0300

    Create constants file

--- src/components/NotehubList/index.tsx
+++ src/components/NotehubList/index.tsx
@@ -1,5 +1,6 @@
 import { useNotehubContext } from "../../contexts/useNotehubContext";
 import { SiStarship } from "react-icons/si";
+import { INITIAL_NOTE_CARD_TEXT } from "../../constants";
 
 export const NotehubList = () => {
   const { noteCards } = useNotehubContext();
@@ -9,7 +10,7 @@ export const NotehubList = () => {
       <div className="max-w-lg px-5 m-auto">
         <h1 className="flex flex-col items-center gap-5 px-5 py-10 text-xl font-bold text-center rounded-xl bg-zinc-900">
           <SiStarship className="text-5xl" />
-          There isn't any notes!
+          {INITIAL_NOTE_CARD_TEXT}
         </h1>
       </div>
     );
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const INITIAL_NOTE_CARD_TEXT = "There isn't any notes!";
